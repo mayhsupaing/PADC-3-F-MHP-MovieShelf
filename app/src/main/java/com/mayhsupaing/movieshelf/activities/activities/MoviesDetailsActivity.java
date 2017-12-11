bac9c9ae6555ed60ba26550ff492206874d7395f
DESKTOP-8HTH83B\Lenovo <mayoosbai@gmail.com>
2017-12-11 11:34:17 +0630

    Movie Details Screen Step 1 -Project Skeleton -Immersive Image

---
+++ app/src/main/java/com/mayhsupaing/movieshelf/activities/activities/MoviesDetailsActivity.java
@@ -0,0 +1,19 @@
+package com.mayhsupaing.movieshelf.activities.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mayhsupaing.movieshelf.R;
+
+/**
+ * Created by Lenovo on 12/11/2017.
+ */
+
+public class MoviesDetailsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies_details);
+    }
+}
